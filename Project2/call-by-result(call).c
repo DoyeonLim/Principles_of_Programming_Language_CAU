1f9b9b5990946e815e590b28c0174377ad072e17
Doyeon Lim <64139953+DoyeonLim@users.noreply.github.com>
2021-10-22 16:22:53 +0900

    Add files via upload

---
+++ Project2/call-by-result(call).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int i = 0;
+int a[2] = { 1, 1 };
+
+void p_value_result_call(int* x, int* y) {
+    int temp1 = *x;
+    int temp2 = *y;
+
+    temp1++; i++; temp2++;
+
+    *x = temp1;
+    *y = temp2;
+}
+
+main() {
+    p_value_result_call(&a[i], &a[i]);
+    printf("reference : a[0]=%d, a[1]=%d\n", a[0], a[1]);
+}
